7168980418bc389aebc3f3908584dc921979315a
Shebin John <admin@remedcu.com>
2019-05-23 15:26:13 +0530

    Added Check Nums Solution

---
+++ Free Challenges/Check Nums.py
@@ -0,0 +1,11 @@
+# Answer to Check Nums
+# https://www.coderbyte.com/information/Check%20Nums
+
+
+def CheckNums(num1,num2):
+    if num1 == num2:
+        return -1
+    return num2 > num1
+
+# keep this function call here
+print CheckNums(raw_input())
